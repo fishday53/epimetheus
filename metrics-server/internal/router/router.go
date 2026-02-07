68e431ad409d52738b1ea3be9e2e44a9d1a746af
a.babushkin <andrey.babushkin@flant.com>
2026-02-07 14:42:56 +0300

    Divide api storages

--- metrics-server/internal/router/router.go
+++ metrics-server/internal/router/router.go
@@ -10,6 +10,11 @@ import (
 
 func NewMultiplexor() *chi.Mux {
 
+	ctx_legacy := &handlers.AppContext{
+		DB:  memory.NewMemStorage(),
+		Log: log.NewLogger(),
+	}
+
 	ctx := &handlers.AppContext{
 		DB:  memory.NewMemStorage(),
 		Log: log.NewLogger(),
@@ -17,19 +22,20 @@ func NewMultiplexor() *chi.Mux {
 
 	r := chi.NewRouter()
 
-	r.Use(ctx.Logger)
-
-	r.Get(`/`, ctx.GetAllParamsJSON)
-
 	// legacy plaintext API
-	r.Get(`/value/{mtype}/{name}`, ctx.GetParam)
-	r.Post(`/update/{mtype}/{name}/{value}`, ctx.SetParam)
+	r.Group(func(r chi.Router) {
+		r.Use(ctx_legacy.Logger)
+		r.Get(`/value/{mtype}/{name}`, ctx_legacy.GetParam)
+		r.Post(`/update/{mtype}/{name}/{value}`, ctx_legacy.SetParam)
+	})
 
 	// JSON API
 	r.Group(func(r chi.Router) {
+		r.Use(ctx.Logger)
 		r.Use(ctx.CheckContentType)
 		r.Post(`/value/`, ctx.GetParamJSON)
 		r.Post(`/update/`, ctx.SetParamJSON)
+		r.Get(`/`, ctx.GetAllParamsJSON)
 	})
 
 	return r
