123adf9644bd88b78032c7ba158f0b35186889cb
a.babushkin <andrey.babushkin@flant.com>
2026-02-28 10:43:20 +0300

    Mv server logic to main

--- metrics-server/internal/server/server.go
+++ metrics-server/internal/server/server.go
@@ -1,55 +1,13 @@
 package server
 
 import (
-	"log"
 	"metrics-server/internal/handlers"
 	"metrics-server/internal/router"
 	"net/http"
-	"os"
-	"time"
-
-	"metrics-server/internal/config"
 )
 
-func Dumper(app *handlers.AppContext) {
-	for {
-		app.DB.Dump(app.Cfg.FileStoragePath)
-		time.Sleep(time.Duration(app.Cfg.StoreInterval) * time.Second)
-	}
-}
-
-func HTTPServer() {
-
-	var err error
-	var cfg config.Config
-
-	log.SetOutput(os.Stdout)
-
-	err = cfg.Get()
-	if err != nil {
-		log.Printf("%v", err)
-		return
-	}
-
-	app, err := handlers.NewAppContext(&cfg)
-	if err != nil {
-		log.Printf("%v", err)
-		return
-	}
-
-	if cfg.Restore {
-		err := app.DB.Restore(cfg.FileStoragePath)
-		if err != nil {
-			app.Log.Fatalf("%v", err)
-			return
-		}
-	}
-
-	if cfg.StoreInterval > 0 {
-		go Dumper(app)
-	}
-
-	err = http.ListenAndServe(cfg.Addr, router.NewMultiplexer(app))
+func HTTPServer(app *handlers.AppContext) {
+	err := http.ListenAndServe(app.Cfg.Addr, router.NewMultiplexer(app))
 	if err != nil {
 		app.Log.Fatalf("%v", err)
 		return
