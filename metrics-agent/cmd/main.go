4d09d63619a4ea10fb24b93c11e29e090ac2eccd
a.babushkin <andrey.babushkin@flant.com>
2026-02-28 11:28:06 +0300

    Mv agent logic to main

--- metrics-agent/cmd/main.go
+++ metrics-agent/cmd/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "metrics-agent/internal/agent"
+import (
+	"log"
+	"metrics-agent/internal/agent"
+	"metrics-agent/internal/config"
+	"os"
+	"time"
+)
 
 func main() {
-	agent.Agent()
+
+	var cfg config.Config
+
+	log.SetOutput(os.Stdout)
+
+	if err := cfg.Get(); err != nil {
+		log.Printf("Cannot get configuration. Error:%v\n", err)
+		return
+	}
+
+	const (
+		proto = "http://"
+		path  = "/update/"
+	)
+	url := proto + cfg.Addr + path
+
+	for {
+		for i := 0; i < (cfg.ReportInterval / cfg.PollInterval); i++ {
+
+			m, err := agent.GetMetrics(&cfg)
+			if err != nil {
+				log.Printf("Cannot get metrics: %v\n", err)
+			}
+
+			for _, mtr := range *m {
+				err = agent.SendMetric(url, mtr)
+				if err != nil {
+					log.Printf("Metric send failed. Metric: %v, Error:%v\n", *mtr, err)
+				}
+			}
+
+			time.Sleep(time.Duration(cfg.PollInterval) * time.Second)
+		}
+	}
 }
