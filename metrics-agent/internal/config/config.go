c25d1199b4aa2aa667c497a62f0df3bf9edb0b6e
a.babushkin <andrey.babushkin@flant.com>
2026-05-02 13:15:28 +0300

    Iter22

--- metrics-agent/internal/config/config.go
+++ metrics-agent/internal/config/config.go
@@ -2,6 +2,7 @@
 package config
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
@@ -11,50 +12,97 @@ import (
 
 // Config is a Metrics-Agent configuration storage.
 type Config struct {
-	Addr           string `env:"ADDRESS"`
-	ReportInterval int    `env:"REPORT_INTERVAL"`
-	PollInterval   int    `env:"POLL_INTERVAL"`
-	HashKey        string `env:"KEY"`
-	RateLimit      int    `env:"RATE_LIMIT"`
+	Addr           string `env:"ADDRESS" json:"address"`
+	ReportInterval int    `env:"REPORT_INTERVAL" json:"report_interval"`
+	PollInterval   int    `env:"POLL_INTERVAL" json:"poll_interval"`
+	HashKey        string `env:"KEY" json:"hash_key"`
+	RateLimit      int    `env:"RATE_LIMIT" json:"rate_limit"`
 	BufferSize     int
-	CryptoKeyPath  string `env:"CRYPTO_KEY"`
+	CryptoKeyPath  string `env:"CRYPTO_KEY" json:"crypto_key"`
+	ConfigPath     string `env:"CONFIG"`
 }
 
 // Get is a single method to get all Metrics-Agent settings.
 func (cfg *Config) Get() error {
+
+	// first: read config file with low priority
+	fs := flag.NewFlagSet("metrics-agent-preread", flag.ContinueOnError)
+	fs.StringVar(&cfg.ConfigPath, "config", "", "Config file path (short: -c)")
+	fs.StringVar(&cfg.ConfigPath, "c", "", "short for -config")
+	fs.Parse(os.Args[1:])
+
+	if config, ok := os.LookupEnv("CONFIG"); ok {
+		cfg.ConfigPath = config
+	}
+
+	if cfg.ConfigPath != "" {
+		if err := cfg.readFromFile(); err != nil {
+			return fmt.Errorf("cannot read config file:%v", err)
+		}
+	}
+
+	// read cmd-line params with medium priority
+	fs = flag.NewFlagSet("metrics-agent", flag.ContinueOnError)
+
+	Addr := fs.String("a", "localhost:8080", "Server address")
+	ReportInterval := fs.Int("r", 10, "Report interval")
+	PollInterval := fs.Int("p", 2, "Poll interval")
+	HashKey := fs.String("k", "", "Hash Key")
+	RateLimit := fs.Int("l", 1, "Rate limit")
+	CryptoKeyPath := fs.String("crypto-key", "", "Public Key path")
+	fs.Parse(os.Args[1:])
+
+	// replace config file options with explicit cmd-line values
+	flag.Visit(func(f *flag.Flag) {
+		switch f.Name {
+		case "a":
+			cfg.Addr = *Addr
+		case "r":
+			cfg.ReportInterval = *ReportInterval
+		case "p":
+			cfg.PollInterval = *PollInterval
+		case "k":
+			cfg.HashKey = *HashKey
+		case "l":
+			cfg.RateLimit = *RateLimit
+		case "crypto-key":
+			cfg.CryptoKeyPath = *CryptoKeyPath
+		}
+	})
+
+	// replace only empty config values with cmd-line defaults
+	if cfg.Addr == "" {
+		cfg.Addr = *Addr
+	}
+	if cfg.ReportInterval == 0 {
+		cfg.ReportInterval = *ReportInterval
+	}
+	if cfg.PollInterval == 0 {
+		cfg.PollInterval = *PollInterval
+	}
+	if cfg.RateLimit == 0 {
+		cfg.RateLimit = *RateLimit
+	}
+
+	// read envs with high priority
 	err := env.Parse(cfg)
 	if err != nil {
 		return fmt.Errorf("config parse error:%v", err)
 	}
 
-	fs := flag.NewFlagSet("metrics-agent", flag.ContinueOnError)
+	return nil
+}
 
-	addr := fs.String("a", "localhost:8080", "Server address")
-	reportInterval := fs.Int("r", 10, "Report interval")
-	pollInterval := fs.Int("p", 2, "Poll interval")
-	hashKey := fs.String("k", "", "Hash Key")
-	rateLimit := fs.Int("l", 1, "Rate limit")
-	cryptoKey := fs.String("crypto-key", "", "Public Key path")
-	fs.Parse(os.Args[1:])
-
-	if cfg.Addr == "" {
-		cfg.Addr = *addr
-	}
-	if cfg.ReportInterval == 0 {
-		cfg.ReportInterval = *reportInterval
-	}
-	if cfg.PollInterval == 0 {
-		cfg.PollInterval = *pollInterval
-	}
-	if cfg.HashKey == "" {
-		cfg.HashKey = *hashKey
-	}
-	if cfg.RateLimit == 0 {
-		cfg.RateLimit = *rateLimit
-	}
-	if cfg.CryptoKeyPath == "" {
-		cfg.CryptoKeyPath = *cryptoKey
+// readFromFile reads config from json changing only unspecified values
+func (cfg *Config) readFromFile() error {
+	cfgData, err := os.ReadFile(cfg.ConfigPath)
+	if err != nil {
+		return fmt.Errorf("error reading file:%v", err)
 	}
 
+	err = json.Unmarshal(cfgData, &cfg)
+	if err != nil {
+		return fmt.Errorf("error parsing json config:%v", err)
+	}
 	return nil
 }
--- metrics-server/internal/config/config_test.go
+++ metrics-server/internal/config/config_test.go
@@ -11,11 +11,13 @@ func Test_Get(t *testing.T) {
 	tests := []struct {
 		testName string
 		envs     map[string]string
+		config   string
 		want     Config
 	}{
 		{
 			testName: "Defaults",
 			envs:     make(map[string]string),
+			config:   "",
 			want: Config{
 				Addr:            "localhost:8080",
 				StoreInterval:   300,
@@ -24,6 +26,7 @@ func Test_Get(t *testing.T) {
 				DSN:             "",
 				HashKey:         "",
 				CryptoKeyPath:   "",
+				ConfigPath:      "",
 			},
 		},
 		{
@@ -36,7 +39,9 @@ func Test_Get(t *testing.T) {
 				"DATABASE_DSN":      "user=postgres password=secret host=localhost port=5432 dbname=mydb sslmode=disable",
 				"KEY":               "secret",
 				"CRYPTO_KEY":        "/path/to/private/key",
+				"CONFIG":            "",
 			},
+			config: "",
 			want: Config{
 				Addr:            "example.com:1234",
 				StoreInterval:   100,
@@ -45,13 +50,83 @@ func Test_Get(t *testing.T) {
 				DSN:             "user=postgres password=secret host=localhost port=5432 dbname=mydb sslmode=disable",
 				HashKey:         "secret",
 				CryptoKeyPath:   "/path/to/private/key",
+				ConfigPath:      "",
+			},
+		},
+		{
+			testName: "Config.json",
+			envs: map[string]string{
+				"CONFIG": os.TempDir() + "config-slucigOmlasBigshAjBadOaweujdeng9.json",
+			},
+			config: `
+				{
+					"address": "example.com:8888",
+					"store_interval": 200,
+					"store_file": "/path/to/dump",
+					"restore": true,
+					"database_dsn": "user=myuser password=pswd host=10.0.0.1 port=6432 dbname=mydb sslmode=enable",
+					"hash_key": "mykey",
+					"crypto_key": "/path/to/key.pem"
+				}
+			`,
+			want: Config{
+				Addr:            "example.com:8888",
+				StoreInterval:   200,
+				FileStoragePath: "/path/to/dump",
+				Restore:         true,
+				DSN:             "user=myuser password=pswd host=10.0.0.1 port=6432 dbname=mydb sslmode=enable",
+				HashKey:         "mykey",
+				CryptoKeyPath:   "/path/to/key.pem",
+				ConfigPath:      os.TempDir() + "config-slucigOmlasBigshAjBadOaweujdeng9.json",
+			},
+		},
+		{
+			testName: "Mixed",
+			envs: map[string]string{
+				"ADDRESS":    "example.com:65000",
+				"RESTORE":    "false",
+				"KEY":        "secret",
+				"CRYPTO_KEY": "/path/to/private/key",
+				"CONFIG":     os.TempDir() + "config-slucigOmlasBigshAjBadOaweujdeng9.json",
+			},
+			config: `
+				{
+					"address": "example.com:8888",
+					"restore": true,
+					"database_dsn": "user=myuser password=pswd host=10.0.0.1 port=6432 dbname=mydb sslmode=enable",
+					"hash_key": "mykey",
+					"crypto_key": "/path/to/key.pem"
+				}
+			`,
+			want: Config{
+				Addr:            "example.com:65000",
+				StoreInterval:   300,
+				FileStoragePath: "metrics.dmp",
+				Restore:         false,
+				DSN:             "user=myuser password=pswd host=10.0.0.1 port=6432 dbname=mydb sslmode=enable",
+				HashKey:         "secret",
+				CryptoKeyPath:   "/path/to/private/key",
+				ConfigPath:      os.TempDir() + "config-slucigOmlasBigshAjBadOaweujdeng9.json",
 			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.testName, func(t *testing.T) {
+			os.Clearenv()
 			for k, v := range tt.envs {
 				os.Setenv(k, v)
+			}
+			if tt.config != "" {
+				tmpFile, err := os.Create(tt.envs["CONFIG"])
+				if err != nil {
+					t.Fatalf("Generate config failed: %v", err)
+				}
+				defer tmpFile.Close()
+				defer os.Remove(tmpFile.Name())
+				_, err = tmpFile.WriteString(tt.config)
+				if err != nil {
+					t.Fatalf("Write config failed: %v", err)
+				}
 			}
 			var cfg Config
 			err := cfg.Get()
